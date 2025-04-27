bb022a60ec1ffe174b0a046a5d2c024eae0dc989
padmavathi-cm <ellapupadma@gmail.com>
2025-04-27 16:05:18 +0530

    method

--- welcome.py
+++ welcome.py
@@ -2,11 +2,9 @@ Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> welcome():
 ...     
-SyntaxError: invalid syntax
->>> welcome()
-Traceback (most recent call last):
-  File "<pyshell#1>", line 1, in <module>
-    welcome()
-NameError: name 'welcome' is not defined
 >>> class welcome:
 ...     print ('hi all')
+
+
+    text()
+       print('method')
